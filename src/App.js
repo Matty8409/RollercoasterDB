a1bcea1d2a766551e8cfd85d8ef0750f05610f17
Matty8409 <mattbartlett840@outlook.com>
2026-06-14 15:25:53 +0100

    MASTER Initial commit

--- src/App.js
+++ src/App.js
@@ -5,21 +5,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>This is a test header</h1>
       </header>
+      <body>
+        This is a test body
+      </body>
+      <button onClick={runTestButton} className='testButton'>CLICK ME!!!!</button>
     </div>
   );
 }
+
+function runTestButton(){
+  alert("HELLO");
+};
 
 export default App;
